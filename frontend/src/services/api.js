6f47a214ae301ffbe658d8529117158cdc5d2374
rucheasy <ruchitabhende2006@gmail.com>
2026-04-26 23:16:23 +0530

    connected backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://fairshare-hrqf.onrender.com/';
 
 export const createGroup = async (name, members) => {
     const response = await axios.post(`${API_URL}/create-group`, { name, members });
